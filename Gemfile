87adacdb86ecec304279aaae0fe19ee44f2c1e4f
Mike Ruby <emikebr@gmail.com>
2013-04-05 18:12:30 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'thin'
+gem 'pg'
+
+# OAuth providers
+gem 'omniauth-twitter'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'compass-rails'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+	gem 'debugger'
+  gem 'quiet_assets'
+end
+
+gem 'compass'
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetSearch::Application.config.secret_token = '30c2370b8367bf838d69a37faa08211e40658d32efecddc330e7cff294b522ec17c478ea09bd719964dbb9df96c97e5ea553a9c65b260527b74ccba8b6f413d7'
