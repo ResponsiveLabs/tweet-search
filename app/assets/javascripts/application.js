6b7369480ee8d41f42e8ccf55bfadec976aa4742
Mike Ruby <emikebr@gmail.com>
2013-04-12 12:11:31 -0500

    Fixes a bug with delete searches

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -99,7 +99,7 @@ function get_searches_saved(){
 		var items = [];
 		$('#words_saved').empty();
 		$.each(data, function(key, val) {
-			items.unshift('<li id="' + val["id"] + '"> <a onclick="remove_search_saved('+ val["id"] +'); return false;" href="#"> x </a> <a onclick=get_results_by("'+ val["word"] +'");>'+ val["word"] +'</a></li>');
+			items.unshift('<li id="' + val["id"] + '"><a onclick=get_results_by("'+ val["word"] +'");>'+ val["word"] +'</a> <a class="delete" onclick="remove_search_saved('+ val["id"] +'); return false;" href="#"> x </a> </li>');
 		});
 		$('<ul/>', {
 			'class': 'tweets_list',
